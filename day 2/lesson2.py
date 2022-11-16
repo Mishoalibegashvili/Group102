01b91bad9e9812f5a4f16a58a654236e69c10564
mishoalibegashvili <mishoalibegashvili@gmail.com>
2022-11-16 16:18:10 +0400

    day2 commit

---
+++ day 2/lesson2.py
@@ -0,0 +1,37 @@
+# name = "misho"
+
+# print(len(name))
+
+# my_sentence = """ბავშვობაში რონალდუს საყვარელი გუნდი ლისაბონის „ბენფიკა“ იყო.
+#  რვა წლის ასაკში, კრიშტიანუ „ანდორინიაში“ ასპარეზობდა, საიდანაც 1995 წელს ის „ნასიონალში“ გადაბარგდა."""
+# print(my_sentence)
+
+
+
+# full_name = "Misho alibegashvili"
+
+# print(len(full_name))
+
+# print(full_name[11])
+
+# print("M" in full_name)
+
+full_name = "Misho alibegashvili"
+
+# print(full_name[2:10])
+# print(full_name[4:7:10])
+# print(full_name[:5])
+# print(full_name[6:])
+
+# print(full_name[-1:-6:-1])
+
+print(full_name.upper())
+
+name2 = ("   misho alibegashvili  ")
+
+# print(name2)
+# name2.strip()
+
+# print(name2.strip())
+
+print(name2.replace("i","#"))
